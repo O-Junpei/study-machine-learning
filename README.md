991a5fc5b22b46e161298401d874b9f94e202bc1
junpei ono <snorlax.chemist.and.jazz@gmail.com>
2019-02-04 00:57:09 +0900

    写経を少し

---
+++ README.md
@@ -0,0 +1,3 @@
+
+# Pythonでmatplotlibをimportするとエラーが出る場合の対処策（Mac）
+https://qiita.com/Gen6/items/78d83d117ef67e0d53c2
---
+++ textbook/chap2.py
@@ -0,0 +1,28 @@
+# Chap2.1
+import numpy as np
+
+
+# 四則演算
+print(1 + 2)
+print((1 + 2 * 3 - 4) / 5)
+
+# 累乗
+print(2 ** 8)
+
+# Chap2.2
+x = 1
+y = 1 / 3
+print(x + y)
+
+Data_1 = 1 / 5
+Data_2 = 3 / 5
+print(Data_1 + Data_2)
+
+# Chap 2.3
+int_type = 1
+float_type = 1.5
+str_type = "leaning"
+bool_type = False
+list_type = [1, 2, 3]
+taple_type = (1, 2, 3)
+ndarray_type = np.array([1, 2, 3])
